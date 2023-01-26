28a9c1d82e21c124ae218825edaf1ac22fe432e1
Kepliter <jxynwsuaf1990@gmail.com>
2023-01-27 01:28:52 +0900

    8

---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test {
+	public static void main (String [] args) {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		int d = 30;
+		System.out.println ("a+b=" +(a+b));
+		System.out.println ("b-a=" +(b-a));
+		System.out.println ("a*b=" +(a*b));
+		System.out.println ("b/a=" +(b/a));
+		System.out.println ("b%a=" +(b%a));
+		System.out.println ("c++=" +(c++));
+		System.out.println ("c--=" +(c--));
+		System.out.println ("d++=" +(d++));
+		System.out.println ("++d=" +(++d));
+	}
+}
---
+++ selfAddMinus.java
@@ -0,0 +1,10 @@
+public class selfAddMinus {
+	public static void main (String[] args) {
+		int a = 2;
+		int b = ++a;
+		int c = 3;
+		int d = --c;
+		System.out.println ("selfAdd="+(b));
+		System.out.println ("selfMinus="+(d));
+	}
+}
